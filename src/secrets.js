ee38bffbb57b5e0000e958b60f867f3dffa01cdf
Rafath21 <rafathmirza05@gmail.com>
2021-11-12 02:58:28 +0530

    Added new logo

---
+++ src/secrets.js
@@ -0,0 +1,7 @@
+export const apiKey="AIzaSyDYbrs6hrSHRLmpX9mVKa72iOuycZ1jcxE";
+export const authDomain= "apple-clone-3ac22.firebaseapp.com";
+export const projectId= "apple-clone-3ac22";
+export const storageBucket= "apple-clone-3ac22.appspot.com";
+export const messagingSenderId= "921508141111";
+export const appId= "1:921508141111:web:0dba93d5d96d78136a87b2";
+export const measurementId= "G-8GXVXKCGJT";
